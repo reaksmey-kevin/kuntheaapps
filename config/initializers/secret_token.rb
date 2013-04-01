3f57747ee0ab86fc59aa7f99b7d772437f99e877
reaksmey <reaksmey@reaksmey-Aspire-V5-471G.(none)>
2013-04-01 09:09:52 +0700

    Initilize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuntheaapps::Application.config.secret_token = '7068cb4afb8e9dae8917fa3a9dc212759f871a8f4d9e6c4a950820a0818cfa484ad3e313e75b5a1f0da79238edc8c931afbd66c6c0b9ce877fe5e726ef2d92d6'
